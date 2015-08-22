a845c130b845fd99d60f9661d1194f8899041b45
hansinla <hansinla@mac.com>
2015-08-22 07:56:48 -0700

    changes to port to angular

--- Ziplines/portfolio/pomodoro/clock.js
+++ Ziplines/portfolio/pomodoro/clock.js
@@ -1,17 +1,30 @@
-var sessionTime = 25;
-var breakTime = 5;
+(function(){
+
+
+	var app= angular.module('clock', []);
+
+	var sessionTime = 25;
+	var breakTime = 5;
+
+	var startTime = Date.now() + sessionTime * 60 * 1000;
+
+	app.controller('TimeCtrl', function($scope, $interval) {
+
+	  var tick = function() {
+	    $scope.clock = startTime - Date.now();
+	  };
+	  
+
+	  tick();
+	  $interval(tick, 1000);
+		
+	});
+
+
+
+})();
 
 function playSound(soundobj) {
   var thissound = document[soundobj];
   thissound.Play();
 }
-
-var module = angular.module('myApp', []);
-
-module.controller('TimeCtrl', function($scope, $interval) {
-  var tick = function() {
-    $scope.clock = Date.now();
-  };
-  tick();
-  $interval(tick, 1000);
-});
